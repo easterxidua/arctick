0b2f4873a22009ac099f63629a625792994dfb9c
kaduanganu <143816872+kaduanganu@users.noreply.github.com>
2026-06-07 03:15:13 +0000

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ const USDC_ABI = [
   "function transfer(address to,uint256 amount) returns (bool)"
 ];
 
-const CONFIG = require("./config");
-
 const express = require('express');
 const app = express();
 
@@ -65,6 +63,49 @@ const CHAIN_MAP = {
   "eth-sepolia": "Ethereum_Sepolia",
   "arbitrum-sepolia": "Arbitrum_Sepolia"
 };
+
+const CHAIN_CONFIG = {
+
+    "arc-testnet": {
+      chainId: "0x4cef52",
+      rpcUrl: "https://rpc.testnet.arc.network",
+      name: "ARC Testnet",
+      explorer: "https://testnet.arcscan.app",
+      usdcAddress: "0x3600000000000000000000000000000000000000"
+    },
+
+    "base-sepolia": {
+      chainId: "0x14a34",
+      rpcUrl: "https://sepolia.base.org",
+      name: "Base Sepolia",
+      explorer: "https://sepolia.basescan.org",
+      usdcAddress: "0x036CbD53842c5426634e7929541eC2318f3dCF7e"
+    },
+
+    "base-mainnet": {
+      chainId: "0x2105",
+      rpcUrl: "https://mainnet.base.org",
+      name: "Base Mainnet",
+      explorer: "https://basescan.org",
+      usdcAddress: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+    },
+
+    "eth-sepolia": {
+      chainId: "0xaa36a7",
+      rpcUrl: "https://sepolia.drpc.org",
+      name: "Ethereum Sepolia",
+      explorer: "https://sepolia.etherscan.io",
+      usdcAddress: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
+    },
+
+    "arbitrum-sepolia": {
+      chainId: "0x66eee",
+      rpcUrl: "https://sepolia-rollup.arbitrum.io/rpc",
+      name: "Arbitrum Sepolia",
+      explorer: "https://sepolia.arbiscan.io",
+      usdcAddress: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d"
+    }
+  };
 
 function jsonBigInt(obj) {
   return JSON.parse(
@@ -337,7 +378,7 @@ app.post('/api/claim', async (req, res) => {
 if (chain === "arc-testnet") {
 
   const provider = new ethers.JsonRpcProvider(
-    CONFIG.chains["arc-testnet"].rpcUrl
+    CHAIN_CONFIG["arc-testnet"].rpcUrl
   );
 
   const treasuryWallet =
@@ -347,7 +388,7 @@ if (chain === "arc-testnet") {
     );
 
   const usdc = new ethers.Contract(
-    CONFIG.chains["arc-testnet"].usdcAddress,
+    CHAIN_CONFIG["arc-testnet"].usdcAddress,
     USDC_ABI,
     treasuryWallet
   );
@@ -453,11 +494,11 @@ app.get("/api/system-balance", async (req, res) => {
   try {
 
     const provider = new ethers.JsonRpcProvider(
-      CONFIG.chains["arc-testnet"].rpcUrl
+      CHAIN_CONFIG["arc-testnet"].rpcUrl
     );
 
     const usdc = new ethers.Contract(
-      CONFIG.chains["arc-testnet"].usdcAddress,
+      CHAIN_CONFIG["arc-testnet"].usdcAddress,
       USDC_ABI,
       provider
     );
