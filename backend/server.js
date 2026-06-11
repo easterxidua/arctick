7edaca86733f3188276a04dab44d9fbc69b1d5c3
easterxi <easterxi@gmail.com>
2026-06-11 07:21:28 +0000

    uhuy

--- backend/server.js
+++ backend/server.js
@@ -350,7 +350,7 @@ const { ethers } = require('ethers');
 require('dotenv').config();
 
 app.use((req, res, next) => {
-  console.log(req.method, req.url);
+  //console.log(req.method, req.url);
   next();
 });
 
@@ -1282,7 +1282,7 @@ app.get('/api/test-bridge-base-to-arc', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-console.log("PORT =", process.env.PORT);
+//console.log("PORT =", process.env.PORT);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Backend running on port ${PORT}`);
