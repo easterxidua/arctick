283a9652ef0daaf90d2cc49a038f84260c324f48
easterxidua <easterxidua@gmail.com>
2026-07-01 03:35:35 +0000

    siplah

--- backend/server.js
+++ backend/server.js
@@ -1946,8 +1946,8 @@ app.get(
 
       const { address } = req.query;
 
-      console.log("address =", req.query.address);
-      console.log("address2 =", address);
+      console.log("XXXaddress =", req.query.address);
+      console.log("XXXaddress2 =", address);
 
       const balance =
         await vault.availableLiquidity();
