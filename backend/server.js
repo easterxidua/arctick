c6b32cb325c856024fb01c9c0570b7f7744ec775
easterxidua <easterxidua@gmail.com>
2026-07-30 03:28:31 +0000

    asedepwes

--- backend/server.js
+++ backend/server.js
@@ -1857,14 +1857,6 @@ app.post('/api/bridge-from-arc', async (req, res) => {
 
     const adapter = getAdapter();
 
-console.log(await signer.provider.getNetwork());
-
-console.log(await provider.getNetwork());
-
-console.log(await signer.provider.getBlockNumber());
-
-console.log(await provider.getBlockNumber());
-
     console.log("calling kit.bridge()");
 
     const result = await kit.bridge({
