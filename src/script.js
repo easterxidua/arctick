364f2e3d63e498be93fad649b55adbe225319ddc
easterxidua <easterxidua@gmail.com>
2026-07-30 06:19:55 +0000

    asedepwes

--- src/script.js
+++ src/script.js
@@ -2506,7 +2506,7 @@ async function showScreen2() {
         <img
             id="cheeeinlogo"
             src="${chainLogo[selectedChain]}"
-            style="width="48" height=auto object-fit:contain; display:block;"
+            style="width="32" height=auto object-fit:contain; display:block;"
         >
 
         <span>●</span>
